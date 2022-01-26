1a83490e0ae25d9c2c217895693904f09b4bf71b
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-01-26 14:35:09 +0000

    code added for app to work - jsx only

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [colour, setColour] = useState("#ffffff");
+  document.body.style.background = colour;
+
+  //--------
+  const randNum = () => {
+    let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    return hexArray[Math.floor(Math.random() * hexArray.length)];
+  };
+
+  const getColour = () => {
+    let hex = "#" + randNum() + randNum() + randNum() + randNum() + randNum() + randNum();
+    return hex;
+
+  };
+
+  //----------------------------
+
+  const handleClick = () => {
+    setColour(getColour);
+  }
+
   return (
-    <div className="App">colour changer</div>
+    <div className="App">
+      <div className="colourBox">
+        <h1>colour is:</h1>
+        <h2>{colour}</h2>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+    </div>
   );
-}
+};
+
 
 export default App;
